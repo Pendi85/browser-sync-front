606853c4f4235a160cab7803c49b4e5a8c9a984c
pendi <elouan.maindron@gmail.com>
2025-05-06 18:47:28 +0200

    Create Login View on extension

---
+++ src/desktop/Login.tsx
@@ -0,0 +1,14 @@
+import './LoginExt.css'
+
+function LoginExt() {
+    return (
+        <>
+            <div className="container">
+                <button>Sign in</button>
+                <button>Create an account</button>
+            </div>
+        </>
+    )
+}
+
+export default LoginExt;
