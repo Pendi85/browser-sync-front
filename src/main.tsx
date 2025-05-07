46f6a06ffe7c944141bffc15e8377936a0bd74e3
pendi <elouan.maindron@gmail.com>
2025-05-07 19:20:20 +0200

    Create Login View on desktop

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LoginExt from './extension/LoginExt.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginExt />
+    <App />
   </StrictMode>,
 )
